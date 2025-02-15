519e538421d35e443fe3e4225ad8de3f004b093c
Rohyj <yacheku@gmail.com>
2025-02-15 19:37:23 +0900

    Member 도메인 및 Role Enum 추가
    - Member 엔티티 생성 (username, password, email, phoneNumber 필드 포함)
    - Role Enum 추가 (ROLE_USER, ROLE_ADMIN)
    - 생성자 및 정적 팩토리 메서드 createMember() 추가

---
+++ src/main/java/com/rserver/miniblog/domain/member/Member.java
@@ -0,0 +1,51 @@
+package com.rserver.miniblog.domain.member;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+
+    @Column(unique = true, nullable = false, length = 13)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private LoginType type;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private Member (String username, String encodePassword, String email, String phoneNumber) {
+        this.username = username;
+        this.password = encodePassword;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.type = LoginType.APP;
+        this.role = Role.ROLE_USER;
+    }
+
+    public static Member createMember (String username, String password, String email, String phoneNumber) {
+        return new Member(username, password, email, phoneNumber);
+    }
+
+}
---
+++ src/main/java/com/rserver/miniblog/domain/member/Role.java
@@ -0,0 +1,8 @@
+package com.rserver.miniblog.domain.member;
+
+import lombok.Getter;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
