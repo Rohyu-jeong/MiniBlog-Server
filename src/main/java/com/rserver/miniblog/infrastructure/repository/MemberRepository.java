2eafa9d9458790abaddde64c2f4e0d48d63d7e73
Rohyj <yacheku@gmail.com>
2025-02-15 19:38:47 +0900

    MemberRepository 추가
    - JPA Repository를 확장하여 Member 엔티티에 대한 기본 CRUD 지원
    - 사용자 중복 확인을 위한 existsByUsername, existsByEmail, existsByPhoneNumber 메서드 추가

---
+++ src/main/java/com/rserver/miniblog/infrastructure/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.rserver.miniblog.infrastructure.repository;
+
+import com.rserver.miniblog.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
+
+}
