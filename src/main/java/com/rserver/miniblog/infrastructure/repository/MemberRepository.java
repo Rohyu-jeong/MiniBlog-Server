6196fc3d3805788a5ed6e532617786e6b7aae6dd
Rohyj <yacheku@gmail.com>
2025-02-18 00:50:51 +0900

    MemberRepository에 findByUsername 메서드 추가
    - username을 기준으로 회원 정보를 조회하는 findByUsername(String username) 메서드 추가
    - Optional<Member>를 반환하여, 존재하지 않을 경우 null 대신 Optional 처리 가능

--- src/main/java/com/rserver/miniblog/infrastructure/repository/MemberRepository.java
+++ src/main/java/com/rserver/miniblog/infrastructure/repository/MemberRepository.java
@@ -4,8 +4,12 @@ import com.rserver.miniblog.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername (String username);
 
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
