237dacf293d7b99cb9766f07d45b547b9d184c81
Rohyj <yacheku@gmail.com>
2025-02-20 01:33:21 +0900

    스케줄링 활성화 추가
    - @EnableScheduling 어노테이션 추가
    - 스케줄링을 활용한 주기적인 작업 수행 가능하도록 설정

--- src/main/java/com/rserver/miniblog/MiniblogApplication.java
+++ src/main/java/com/rserver/miniblog/MiniblogApplication.java
@@ -2,8 +2,10 @@ package com.rserver.miniblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MiniblogApplication {
 
 	public static void main(String[] args) {
