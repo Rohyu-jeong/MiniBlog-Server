0f269001511300639f77cb384971a502bae91980
Rohyj <yacheku@naver.com>
2025-03-16 00:52:15 +0900

    refactor: TokenResponseDto 파일명을 AuthToken으로 변경
    - TokenResponseDto.java 삭제
    - AuthToken.java 추가
    - 기존 응답 DTO의 역할을 유지하며 파일명 수정

---
+++ src/main/java/com/rserver/miniblog/application/dto/AuthToken.java
@@ -0,0 +1,20 @@
+package com.rserver.miniblog.application.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthToken {
+
+    private final String accessToken;
+    private final String refreshToken;
+
+    public static AuthToken of(String accessToken, String refreshToken) {
+        return new AuthToken(accessToken, refreshToken);
+    }
+
+}
