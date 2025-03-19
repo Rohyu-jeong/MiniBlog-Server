70a80259be1d05f4a4fa953ddfadcf6ec94c0b2a
Rohyj <yacheku@naver.com>
2025-03-19 12:23:30 +0900

    refactor: PasswordUpdateRequestDTO에서 DTO 접미사 제거

---
+++ src/main/java/com/rserver/miniblog/application/dto/request/PasswordUpdateRequest.java
@@ -0,0 +1,24 @@
+package com.rserver.miniblog.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class PasswordUpdateRequest {
+
+    @NotBlank(message = "현재 비밀번호를 입력해주세요.")
+    private String currentPassword;
+
+    @NotBlank(message = "새 비밀번호를 입력해주세요.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "비밀번호는 최소 하나의 영문자, 하나의 숫자, 하나의 특수문자(@$!%*?&)를 포함해야 합니다."
+    )
+    private String newPassword;
+
+}
