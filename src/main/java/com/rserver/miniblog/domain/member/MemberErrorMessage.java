efd5c4a21385ad8fd1e666bcbf72e6bddb6ba9b7
Rohyj <yacheku@naver.com>
2025-03-20 00:32:04 +0900

    refactor: MemberErrorMessage 리팩토링
    - BaseMessage 인터페이스를 구현하여 일관된 메시지 구조 적용
    - 기존 단순 메시지 구조에서 HttpStatus 포함하도록 개선
    - 예외 발생 시 상태 코드(HttpStatus)를 활용 가능하도록 변경

--- src/main/java/com/rserver/miniblog/domain/member/MemberErrorMessage.java
+++ src/main/java/com/rserver/miniblog/domain/member/MemberErrorMessage.java
@@ -1,18 +1,21 @@
 package com.rserver.miniblog.domain.member;
 
+import com.rserver.miniblog.common.BaseMessage;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
-public enum MemberErrorMessage {
-    MEMBER_NOT_FOUND("멤버 정보를 찾을 수 없습니다."),
-    NICKNAME_NOT_FOUND("닉네임을 찾을 수 없습니다."),
-    PASSWORD_NOT_MISMATCH("현재 비밀번호가 일치하지 않습니다."),
-    USERNAME_DUPLICATE("이미 사용중인 아이디입니다."),
-    EMAIL_DUPLICATE("이미 사용중인 이메일입니다."),
-    PHONE_DUPLICATE("이미 사용중인 전화번호입니다."),
-    NICKNAME_DUPLICATE("이미 사용중인 닉네임입니다.");
+public enum MemberErrorMessage implements BaseMessage {
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버 정보를 찾을 수 없습니다."),
+    NICKNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "닉네임을 찾을 수 없습니다."),
+    PASSWORD_NOT_MISMATCH(HttpStatus.BAD_REQUEST, "현재 비밀번호가 일치하지 않습니다."),
+    USERNAME_DUPLICATE(HttpStatus.CONFLICT, "이미 사용중인 아이디입니다."),
+    EMAIL_DUPLICATE(HttpStatus.CONFLICT, "이미 사용중인 이메일입니다."),
+    PHONE_DUPLICATE(HttpStatus.CONFLICT, "이미 사용중인 전화번호입니다."),
+    NICKNAME_DUPLICATE(HttpStatus.CONFLICT, "이미 사용중인 닉네임입니다.");
 
+    private final HttpStatus status;
     private final String message;
 }
