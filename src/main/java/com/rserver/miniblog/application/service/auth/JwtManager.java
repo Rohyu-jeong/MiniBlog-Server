19e51547a3b4a86febb854e48d332db48c9590ce
Rohyj <yacheku@naver.com>
2025-03-19 12:31:49 +0900

    refactor: 네이밍 변경에 따른 코드 반영

--- src/main/java/com/rserver/miniblog/application/service/auth/JwtManager.java
+++ src/main/java/com/rserver/miniblog/application/service/auth/JwtManager.java
@@ -1,17 +1,14 @@
 package com.rserver.miniblog.application.service.auth;
 
-import com.rserver.miniblog.application.dto.internal.JwtClaims;
-import com.rserver.miniblog.application.dto.internal.IssueTokenInfo;
+import com.rserver.miniblog.application.dto.internal.TokenMemberData;
+import com.rserver.miniblog.application.dto.internal.IssueTokenData;
 import com.rserver.miniblog.application.dto.AuthToken;
 import com.rserver.miniblog.application.service.member.MemberService;
 import com.rserver.miniblog.domain.member.Member;
 import com.rserver.miniblog.domain.token.RefreshToken;
-import com.rserver.miniblog.exception.InvalidTokenException;
 import com.rserver.miniblog.infrastructure.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -23,8 +20,8 @@ public class JwtManager implements AuthTokenManager{
     private final JwtProvider jwtProvider;
 
     @Override
-    public AuthToken issueToken(IssueTokenInfo issueTokenInfo) {
-        JwtClaims claims = JwtClaims.from(issueTokenInfo.getMember(), issueTokenInfo.getDeviceInfo(), issueTokenInfo.getIpAddress());
+    public AuthToken issueToken(IssueTokenData issueTokenData) {
+        TokenMemberData claims = TokenMemberData.from(issueTokenData.getMember(), issueTokenData.getDeviceInfo(), issueTokenData.getIpAddress());
 
         String accessToken = jwtProvider.generateAccessToken(claims);
         String refreshToken = jwtProvider.generateRefreshToken(claims.getUsername(), claims.getMemberId());
@@ -35,7 +32,7 @@ public class JwtManager implements AuthTokenManager{
     }
 
     @Override
-    public IssueTokenInfo validateRefreshToken(String refreshToken) {
+    public IssueTokenData validateRefreshToken(String refreshToken) {
         blacklistService.isTokenBlacklisted(refreshToken);
 
         RefreshToken token = refreshTokenService.find(refreshToken);
@@ -44,7 +41,7 @@ public class JwtManager implements AuthTokenManager{
 
         Member member = memberService.findMember(token.getMemberId());
 
-        return IssueTokenInfo.from(member, token.getDeviceInfo(), token.getIpAddress());
+        return IssueTokenData.from(member, token.getDeviceInfo(), token.getIpAddress());
     }
 
     @Override
