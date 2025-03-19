048d605e596b48bb4581a3fcce611014f7f7a373
Rohyj <yacheku@naver.com>
2025-03-20 01:55:49 +0900

    fix: @Validated 애너테이션 삭제

--- src/main/java/com/rserver/miniblog/presentaion/controller/AuthController.java
+++ src/main/java/com/rserver/miniblog/presentaion/controller/AuthController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
-@Validated
 @RequiredArgsConstructor
 public class AuthController {
 
