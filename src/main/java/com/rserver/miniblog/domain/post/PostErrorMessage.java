e8f41520d6128661424fe50c06b4fadd34f1b620
Rohyj <yacheku@naver.com>
2025-03-20 01:37:58 +0900

    refactor: @Getter와 @RequiredArgsConstructor 순서 변경

--- src/main/java/com/rserver/miniblog/domain/post/PostErrorMessage.java
+++ src/main/java/com/rserver/miniblog/domain/post/PostErrorMessage.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-@Getter
 @RequiredArgsConstructor
+@Getter
 public enum PostErrorMessage implements BaseMessage {
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
