a527ff2a35e52b1743eaa371898234601701b5cf
Rohyj <yacheku@naver.com>
2025-03-21 12:32:36 +0900

    refactor: TokenErrorMessage의 status 타입을 HttpStatus에서 int로 변경

--- src/main/java/com/rserver/miniblog/domain/token/TokenErrorMessage.java
+++ src/main/java/com/rserver/miniblog/domain/token/TokenErrorMessage.java
@@ -8,13 +8,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum TokenErrorMessage implements BaseMessage {
-    TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "Refresh Token 정보를 찾을 수가 없습니다."),
-    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
-    TOKEN_MISMATCH(HttpStatus.UNAUTHORIZED, "리프레시 토큰이 일치하지 않습니다."),
-    DEVICE_INFO_MISMATCH(HttpStatus.UNAUTHORIZED, "디바이스 정보가 일치하지 않습니다."),
-    IP_MISMATCH(HttpStatus.UNAUTHORIZED, "IP 주소가 일치하지 않습니다."),
-    BLACKLISTED_TOKEN(HttpStatus.FORBIDDEN, "블랙리스트에 등록된 토큰입니다.");
+    TOKEN_NOT_FOUND(404, "Refresh Token 정보를 찾을 수가 없습니다."),
+    TOKEN_EXPIRED(401, "토큰이 만료되었습니다."),
+    TOKEN_MISMATCH(401, "리프레시 토큰이 일치하지 않습니다."),
+    DEVICE_INFO_MISMATCH(401, "디바이스 정보가 일치하지 않습니다."),
+    IP_MISMATCH(401, "IP 주소가 일치하지 않습니다."),
+    BLACKLISTED_TOKEN(403, "블랙리스트에 등록된 토큰입니다.");
 
-    private final HttpStatus status;
+    private final int status;
     private final String message;
 }
