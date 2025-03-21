19a21c00160d3eae9beabe2a1ecab2eaad0a0990
Rohyj <yacheku@naver.com>
2025-03-21 12:32:00 +0900

    refactor: MemberErrorMessage의 status 타입을 HttpStatus에서 int로 변경

--- src/main/java/com/rserver/miniblog/domain/member/MemberErrorMessage.java
+++ src/main/java/com/rserver/miniblog/domain/member/MemberErrorMessage.java
@@ -8,14 +8,14 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum MemberErrorMessage implements BaseMessage {
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "멤버 정보를 찾을 수 없습니다."),
-    NICKNAME_NOT_FOUND(HttpStatus.NOT_FOUND, "닉네임을 찾을 수 없습니다."),
-    PASSWORD_NOT_MISMATCH(HttpStatus.BAD_REQUEST, "현재 비밀번호가 일치하지 않습니다."),
-    USERNAME_DUPLICATE(HttpStatus.CONFLICT, "이미 사용중인 아이디입니다."),
-    EMAIL_DUPLICATE(HttpStatus.CONFLICT, "이미 사용중인 이메일입니다."),
-    PHONE_DUPLICATE(HttpStatus.CONFLICT, "이미 사용중인 전화번호입니다."),
-    NICKNAME_DUPLICATE(HttpStatus.CONFLICT, "이미 사용중인 닉네임입니다.");
+    MEMBER_NOT_FOUND(404, "멤버 정보를 찾을 수 없습니다."),
+    NICKNAME_NOT_FOUND(404, "닉네임을 찾을 수 없습니다."),
+    PASSWORD_NOT_MISMATCH(400, "현재 비밀번호가 일치하지 않습니다."),
+    USERNAME_DUPLICATE(409, "이미 사용중인 아이디입니다."),
+    EMAIL_DUPLICATE(409, "이미 사용중인 이메일입니다."),
+    PHONE_DUPLICATE(409, "이미 사용중인 전화번호입니다."),
+    NICKNAME_DUPLICATE(409, "이미 사용중인 닉네임입니다.");
 
-    private final HttpStatus status;
+    private final int status;
     private final String message;
 }
