c4407c1ee1e6de844fcd0ec3a7fecf37c68df9b1
Rohyj <yacheku@naver.com>
2025-03-21 12:36:18 +0900

    refactor: 예외 처리 응답 코드 타입 변경
    - ApiResponse.error()에서 HttpStatus의 value() 대신 int 타입의 상태 코드 직접 사용
    - 예외 핸들러에서 일관성을 유지하기 위해 `HttpStatus.BAD_REQUEST.value()` → `400` 등으로 변경
    - BaseErrorException, BadCredentialsException, Exception 등의 예외 처리 로직 수정

--- src/main/java/com/rserver/miniblog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rserver/miniblog/exception/GlobalExceptionHandler.java
@@ -17,22 +17,22 @@ public class GlobalExceptionHandler {
                 .findFirst()
                 .orElse("요청한 값이 올바르지 않습니다.");
 
-        return ApiResponse.error(HttpStatus.BAD_REQUEST.value(), errorMessage);
+        return ApiResponse.error(400, errorMessage);
     }
 
     @ExceptionHandler(BaseErrorException.class)
     public ApiResponse<Void> handleCustomExceptions(BaseErrorException ex) {
-        return ApiResponse.error(ex.getErrorMessage().getStatus().value(), ex.getErrorMessage().getMessage());
+        return ApiResponse.error(ex.getErrorMessage().getStatus(), ex.getErrorMessage().getMessage());
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ApiResponse<Void> handleBadCredentialsException(BadCredentialsException ex) {
-        return ApiResponse.error(HttpStatus.UNAUTHORIZED.value(), "로그인 정보가 일치하지 않습니다.");
+        return ApiResponse.error(401, "로그인 정보가 일치하지 않습니다.");
     }
 
 
     @ExceptionHandler(Exception.class)
     public ApiResponse<Void> handleAllExceptions(Exception ex) {
-        return ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.value(), "내부 서버 오류가 발생했습니다.");
+        return ApiResponse.error(500, "내부 서버 오류가 발생했습니다.");
     }
 }
