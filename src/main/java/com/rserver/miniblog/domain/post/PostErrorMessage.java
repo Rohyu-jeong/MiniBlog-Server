08016f2493f9bab22be3ecccabe662052d5e9b24
Rohyj <yacheku@naver.com>
2025-03-21 12:32:17 +0900

    refactor: PostErrorMessage의 status 타입을 HttpStatus에서 int로 변경

--- src/main/java/com/rserver/miniblog/domain/post/PostErrorMessage.java
+++ src/main/java/com/rserver/miniblog/domain/post/PostErrorMessage.java
@@ -8,15 +8,15 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 @Getter
 public enum PostErrorMessage implements BaseMessage {
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다."),
-    COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "댓글을 찾을 수 없습니다."),
-    NOT_POST_OWNER(HttpStatus.UNAUTHORIZED, "본인이 작성한 게시글만 수정할 수 있습니다."),
-    NOT_COMMENT_OWNER(HttpStatus.UNAUTHORIZED, "본인의 댓글만 수정할 수 있습니다."),
-    INVALID_COMMENT(HttpStatus.BAD_REQUEST, "해당 게시글의 댓글이 아닙니다."),
-    IMAGE_UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드 실패"),
-    INVALID_IMAGE_FORMAT(HttpStatus.BAD_REQUEST, "잘못된 이미지 형식입니다."),
-    IMAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "이미지를 찾을 수 없습니다.");
+    POST_NOT_FOUND(404, "게시글을 찾을 수 없습니다."),
+    COMMENT_NOT_FOUND(404, "댓글을 찾을 수 없습니다."),
+    NOT_POST_OWNER(401, "본인이 작성한 게시글만 수정할 수 있습니다."),
+    NOT_COMMENT_OWNER(401, "본인의 댓글만 수정할 수 있습니다."),
+    INVALID_COMMENT(400, "해당 게시글의 댓글이 아닙니다."),
+    IMAGE_UPLOAD_FAILED(500, "이미지 업로드 실패"),
+    INVALID_IMAGE_FORMAT(400, "잘못된 이미지 형식입니다."),
+    IMAGE_NOT_FOUND(404, "이미지를 찾을 수 없습니다.");
 
-    private final HttpStatus status;
+    private final int status;
     private final String message;
 }
