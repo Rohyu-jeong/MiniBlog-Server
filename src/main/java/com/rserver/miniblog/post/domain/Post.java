e1249f0f6ae0dd4d9025807356b25ab11ec8de8a
Rohyj <yacheku@naver.com>
2025-03-24 13:23:52 +0900

    게시글 도메인 엔티티 Post 추가
    - 게시글 ID, 회원 ID, 제목, 내용, 이미지 URL, 생성/수정 시간 필드 정의
    - 정적 팩토리 메서드 createPost, 게시글 수정 메서드 updatePost 추가

---
+++ src/main/java/com/rserver/miniblog/post/domain/Post.java
@@ -0,0 +1,59 @@
+package com.rserver.miniblog.post.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Post {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    @Column(nullable = false, columnDefinition = "LONGTEXT")
+    private String content;
+
+    @Column(columnDefinition = "LONGTEXT")
+    private String imageUrl;
+
+    @Column(nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+
+    private Post(Long memberId, String title, String content, String imageUrl) {
+        this.memberId = memberId;
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+    }
+
+    public static Post createPost(Long memberId, String title, String content, String imageUrl) {
+        return new Post(memberId, title, content, imageUrl);
+    }
+
+    public void updatePost(String newTitle, String newContent, String newImageUrl) {
+        this.title = newTitle;
+        this.content = newContent;
+        this.imageUrl = newImageUrl;
+    }
+
+}
