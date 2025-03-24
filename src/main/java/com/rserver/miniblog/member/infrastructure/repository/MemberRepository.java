178798371dbd9c6b2073590018e8872de97c6e79
Rohyj <yacheku@naver.com>
2025-03-24 13:15:29 +0900

    MemberRepository 인터페이스 정의
    - 사용자 정보 조회 메서드 정의 (findByUsername, findNicknameByUsername)
    - 중복 검사 메서드 추가 (아이디, 이메일, 전화번호, 닉네임)

---
+++ src/main/java/com/rserver/miniblog/member/infrastructure/repository/MemberRepository.java
@@ -0,0 +1,24 @@
+package com.rserver.miniblog.member.infrastructure.repository;
+
+import com.rserver.miniblog.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByPhoneNumber(String phoneNumber);
+    boolean existsByNickname(String nickname);
+
+    @Query("SELECT m.nickname FROM Member m WHERE m.username = :username")
+    Optional<String> findNicknameByUsername(@Param("username") String username);
+
+}
