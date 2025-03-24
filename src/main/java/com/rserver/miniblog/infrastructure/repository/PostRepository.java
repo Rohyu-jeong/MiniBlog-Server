b9efbe5752d0ae5e8a53f02c07b52e1e0dae7a6b
Rohyj <yacheku@naver.com>
2025-03-24 12:39:11 +0900

    패키지 구조 변경을 위한 기존 클래스 삭제

--- src/main/java/com/rserver/miniblog/infrastructure/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.rserver.miniblog.infrastructure.repository;
-
-import com.rserver.miniblog.domain.post.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
