dbccaedbe6db762be37d9990bc062562650dfee8
Rohyj <yacheku@naver.com>
2025-03-24 13:09:53 +0900

    API 응답 공통 포맷 클래스 ApiResponse 추가
    - 응답 일관성을 위한 제네릭 ApiResponse 클래스 추가
    - 성공/실패 응답 메서드 정의 (success(), error())

---
+++ src/main/java/com/rserver/miniblog/common/ApiResponse.java
@@ -0,0 +1,30 @@
+package com.rserver.miniblog.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import static com.rserver.miniblog.common.SuccessMessage.*;
+
+@RequiredArgsConstructor
+@Getter
+public class ApiResponse<T> {
+
+    private final int status;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private final T data;
+
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(SUCCESS.getStatus(), SUCCESS.getMessage(), data);
+    }
+
+    public static <T> ApiResponse<T> success() {
+        return new ApiResponse<>(SUCCESS.getStatus(), SUCCESS.getMessage(), null);
+    }
+
+    public static <T> ApiResponse<T> error(int status, String message) {
+        return new ApiResponse<>(status, message, null);
+    }
+
+}
