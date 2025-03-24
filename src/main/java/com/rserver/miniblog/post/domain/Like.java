1851c06b925a4dadeed2eb349d86fdf7306170a6
Rohyj <yacheku@naver.com>
2025-03-24 13:24:23 +0900

    좋아요 도메인 엔티티 Like 추가
    - 회원 ID와 게시글 ID로 구성된 유니크 제약 조건 설정
    - 정적 팩토리 메서드 createLike 추가

---
+++ src/main/java/com/rserver/miniblog/post/domain/Like.java
@@ -0,0 +1,36 @@
+package com.rserver.miniblog.post.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "likes", uniqueConstraints = @UniqueConstraint(
+        columnNames = {"memberId", "postId"}
+))
+public class Like {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "like_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    private Like(Long memberId, Long postId) {
+        this.memberId = memberId;
+        this.postId = postId;
+    }
+
+    public static Like createLike(Long memberId, Long postId) {
+        return new Like(memberId, postId);
+    }
+
+}
