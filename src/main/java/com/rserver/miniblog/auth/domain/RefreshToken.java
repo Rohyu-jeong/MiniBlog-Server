dc0c318e6b47f0fba93f9b02365efb352c9e04b8
Rohyj <yacheku@naver.com>
2025-03-24 12:46:07 +0900

    auth 패키지 내 RefreshToken 도메인 클래스 추가 및 관련 로직 정리
    - RefreshToken 엔티티 생성
    - 토큰 유효성 검사 로직 포함 (validateToken, validateDeviceInfo 등)

---
+++ src/main/java/com/rserver/miniblog/auth/domain/RefreshToken.java
@@ -0,0 +1,85 @@
+package com.rserver.miniblog.auth.domain;
+
+import com.rserver.miniblog.exception.InvalidTokenException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+import static com.rserver.miniblog.auth.domain.TokenErrorMessage.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "refresh_token_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false, length = 512)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private String deviceInfo;
+
+    @Column(nullable = false, length = 45)
+    private String ipAddress;
+
+    @Column(nullable = false)
+    private LocalDateTime expireAt;
+
+    private RefreshToken(Long memberId, String refreshToken, String deviceInfo, String ipAddress, LocalDateTime expireAt) {
+        this.memberId = memberId;
+        this.refreshToken = refreshToken;
+        this.deviceInfo = deviceInfo;
+        this.ipAddress = ipAddress;
+        this.expireAt = expireAt;
+    }
+
+    public static RefreshToken create(Long memberId, String refreshToken, String deviceInfo, String ipAddress, long refreshTokenExpirationMillis) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime expireAt = now.plus(refreshTokenExpirationMillis, ChronoUnit.MILLIS);
+
+        return new RefreshToken(memberId, refreshToken, deviceInfo, ipAddress, expireAt);
+    }
+
+    public void validate(String token, String deviceInfo, String ipAddress) {
+        validateToken(token);
+        validateDeviceInfo(deviceInfo);
+        validateIpAddress(ipAddress);
+    }
+
+    public void validateToken() {
+        if (LocalDateTime.now().isAfter(expireAt)) {
+            throw new InvalidTokenException(TOKEN_EXPIRED);
+        }
+    }
+
+    private void validateToken(String token) {
+        if (!this.refreshToken.equals(token)) {
+            throw new InvalidTokenException(TOKEN_MISMATCH);
+        }
+    }
+
+    private void validateDeviceInfo(String deviceInfo) {
+        if (!this.deviceInfo.equals(deviceInfo)) {
+            throw new InvalidTokenException(DEVICE_INFO_MISMATCH);
+        }
+    }
+
+    private void validateIpAddress(String ipAddress) {
+        if (!this.ipAddress.equals(ipAddress)) {
+            throw new InvalidTokenException(IP_MISMATCH);
+        }
+    }
+
+}
